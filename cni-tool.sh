899b613821a0f1a53cc81a5ad93c71bdcad4949a
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-02-24 18:00:40 -0500

    Update cni-tool.sh

--- cni-tool.sh
+++ cni-tool.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Define the version of CNI plugins to install
 CNI_PLUGINS_VERSION="v1.4.0"
 
@@ -10,55 +12,28 @@ CNI_PLUGINS_URL="https://github.com/containernetworking/plugins/releases/downloa
 CNI_TARGET_DIR="/opt/cni/bin"
 
 echo "Downloading CNI plugins version ${CNI_PLUGINS_VERSION}..."
-wget -O /tmp/cni-plugins.tgz "${CNI_PLUGINS_URL}"
-
-if [ $? -ne 0 ]; then
-    echo "Failed to download CNI plugins."
-    exit 1
-fi
+curl --fail -Lo /tmp/cni-plugins.tgz "${CNI_PLUGINS_URL}"
 
 echo "Creating target directory at ${CNI_TARGET_DIR}..."
 sudo mkdir -p "${CNI_TARGET_DIR}"
 
 echo "Extracting CNI plugins to ${CNI_TARGET_DIR}..."
 sudo tar -xvzf /tmp/cni-plugins.tgz -C "${CNI_TARGET_DIR}"
-
-if [ $? -eq 0 ]; then
-    echo "CNI plugins have been installed successfully."
-else
-    echo "Failed to extract CNI plugins."
-    exit 1
-fi
+echo "CNI plugins have been installed successfully."
 
 # Cleanup
 rm /tmp/cni-plugins.tgz
 
 ## Define path in bashrc
-echo "export CNI_PATH=/opt/cni/bin" >> ~/.bashrc
-
-# Add or update cron job to ensure cniVersion remains "1.0.0"
-
-CRON_JOB="@hourly for FILE in /home/derrik/.config/cni/net.d/*.conflist; do sudo sed -i 's/\"cniVersion\": \"1.0.0\"/\"cniVersion\": \"0.4.0\"/g' \$FILE; done"
-
-# Write out current crontab to a temporary file
-crontab -l > mycron
-
-# Echo new cron job into the cron file, removing any old job related to this script
-grep -v 'cniVersion' mycron > mycron.tmp && mv mycron.tmp mycron
-echo "$CRON_JOB" >> mycron
-
-# Install new cron jobs from the file
-crontab mycron
-rm mycron
-
-echo "Cron job added to maintain cniVersion at 1.0.0"
-
+if ! grep -q 'export CNI_PATH=/opt/cni/bin' ~/.bashrc; then
+    echo "export CNI_PATH=/opt/cni/bin" >> ~/.bashrc
+fi
 
 # Define the directory containing CNI config files
 CNI_CONFIG_DIR="/home/derrik/.config/cni/net.d/"
 
 # Define the old and new CNI version values
-OLD_CNI_VERSION="\"cniVersion\": \"1.0.0\""
+OLD_CNI_VERSION="\"cniVersion\": \"0.4.0\""
 NEW_CNI_VERSION="\"cniVersion\": \"0.4.0\""
 
 # Check if the CNI configuration directory exists
@@ -77,3 +52,11 @@ else
 fi
 
 echo "CNI plugin installation and configuration update complete. CNI plugins are located in ${CNI_TARGET_DIR}"
+
+# Add or update cron job to ensure cniVersion remains "0.4.0"
+CRON_JOB="@hourly for FILE in ${CNI_CONFIG_DIR}*.conflist; do sudo sed -i 's/\"cniVersion\": \".*\"/\"cniVersion\": \"0.4.0\"/g' \$FILE; done"
+
+# Check if the cron job exists, and add it if it doesn't
+(crontab -l 2>/dev/null | grep -Fv "cniVersion"; echo "$CRON_JOB") | crontab -
+
+echo "Cron job added to maintain cniVersion at 0.4.0"
