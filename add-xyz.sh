8687066ed78b498faa954e5eea31be0372e66b63
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-05-29 04:33:51 -0400

    Update add-xyz.sh

--- add-xyz.sh
+++ add-xyz.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
 
 # Variables
-ISO_URL="https://boot.netboot.xyz/ipxe/netboot.xyz.iso"
-ISO_PATH="/boot/iso/netboot.xyz.iso"
+LKR_URL="https://boot.netboot.xyz/ipxe/netboot.xyz.lkrn"
+LKR_PATH="/boot/netboot.xyz.lkrn"
 GRUB_CUSTOM_FILE="/etc/grub.d/40_custom"
 GRUB_CFG_FILE="/boot/grub/grub.cfg"
 
-# Download the latest Netboot.xyz ISO
-echo "Downloading Netboot.xyz ISO..."
-sudo mkdir -p /boot/iso
-sudo wget -O $ISO_PATH $ISO_URL
+# Download the latest Netboot.xyz lkrn file
+echo "Downloading Netboot.xyz lkrn file..."
+sudo wget -O $LKR_PATH $LKR_URL
 
 # Create a custom GRUB menu entry
 echo "Adding custom GRUB entry..."
 sudo sh -c "cat >> $GRUB_CUSTOM_FILE" <<EOL
 
 menuentry "Netboot.xyz" {
-    set iso_path="$ISO_PATH"
-    loopback loop \$iso_path
-    linux (loop)/boot/netboot.xyz-linux boot=live iso-scan/filename=\$iso_path
-    initrd (loop)/boot/netboot.xyz-initrd.img
+    linux16 $LKR_PATH
 }
 EOL
 
